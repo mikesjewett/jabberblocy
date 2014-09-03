1f40eaaaea80c828b28ac4264ef23e9f5f91db30
Mike Jewett <mikesjewett@gmail.com>
2014-09-03 12:43:07 -0400

    Managing message data with collections

--- client/views/messages/message_list.js
+++ client/views/messages/message_list.js
@@ -1,18 +1,5 @@
-messageData = [
-  {
-    name:    "Joe Lipper",
-    content: "Wow building a chat app with Meteor is so easy!"
-  },
-  {
-    name:    "Mike Jewett",
-    content: "Yeah wow! What a great framework. I can't wait to keep building this thing out!"
-  },
-  {
-    name:    "Joe Lipper",
-    content: "Hang in there -- we've only scratched the surface on this thing."
-  }
-];
-
 Template.messageList.helpers({
-  messages: messageData
+  messages: function() {
+    return Messages.find();
+  }
 });
